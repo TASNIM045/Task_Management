268a2c80c9d18c1d72b3721411753f0fcf29dd73
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-06-21 00:01:33 +0600

    done

--- task_management/settings.py
+++ task_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,15 +91,23 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # }
 
 # For postgres
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'task_management',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'task_management',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://task_maneger_db_user:GfYej1LyEFYt0BTBKNRWyx3cM4KAJpHC@dpg-d1apm695pdvs73d5h7ug-a.oregon-postgres.render.com/task_maneger_db',
+        conn_max_age=600
+    ) 
 }
 
 
